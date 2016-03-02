50c321d271820dc91d2941d0ce656cb2f16fb4b1
circlespainter <fabio@paralleluniverse.co>
2016-03-02 12:21:26 +0200

    Reuse address for Jetty

--- libs/servers/src/main/java/co/paralleluniverse/comsat/bench/http/server/standalone/Jetty.java
+++ libs/servers/src/main/java/co/paralleluniverse/comsat/bench/http/server/standalone/Jetty.java
@@ -56,6 +56,7 @@ public final class Jetty {
     private static Server server(int port, int backlog, int maxProcessingP) {
         final Server s = new Server(new QueuedThreadPool(maxProcessingP, Runtime.getRuntime().availableProcessors()));
         final ServerConnector http = new ServerConnector(s);
+        http.setReuseAddress(true);
         http.setIdleTimeout(ServerUtils.TIMEOUT);
         http.setAcceptQueueSize(backlog);
         http.setPort(port);
