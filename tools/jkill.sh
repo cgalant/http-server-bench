df7b1a9b191e6440f6a0537e64deec16445e7e36
circlespainter <fabio@paralleluniverse.co>
2016-03-07 08:15:14 +0200

    Cleanup

--- tools/jkill.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# only uses first 15 characters ...
-pgrep __judo_helper.s | xargs -Ixxx kill -2 -xxx
-
--- tools/ulim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run the commands wrapped in a ulimit
-
-cmd="$@"
-
-user=$(whoami)
-
-sudo bash -c "ulimit -n 102400; su $user -mc '$cmd'"
